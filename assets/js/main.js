9317aa4dfd5ace5017e058739b1a8a4bc45a3b06
rodrigoalgeri <72094981+rodrigoalgeri@users.noreply.github.com>
2025-03-27 17:40:19 -0400

    Implementação inicial do header
    - Adicionado cabeçalho responsivo com menu hamburger animado
    - Implementado alternador de tema claro/escuro
    - Criado campo de busca com design moderno
    - Adicionado botão de login
    - Configurado layout base com gradiente de fundo
    - Implementado suporte para modo escuro

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+document.getElementById('themeToggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    const icon = this.querySelector('i');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+});
